c8e0b9507670c5d3b874754466d48cf5a6efc80f
Aliaxei Hlushakou <alexeyka981@gmail.com>
2018-09-26 13:22:21 +0300

    not good but

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+ module.exports = function check(str, bracketsConfig) {
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+ 
+  arr = str.split("");
+  let bracketsDictionary = {};
+  for (let i = 0; i < bracketsConfig.length; i++){
+    const key = bracketsConfig[i][0];
+    const value = bracketsConfig[i][1];
+    bracketsDictionary[key] = value;
+  }
+
+  let stack = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Object.keys(bracketsDictionary).find(key => key === arr[i])) {
+      stack.push(arr[i]);
+      if(arr[i] == '8'){
+        stack.pop();
+      }
+      if (arr[i] == '7'){
+        stack.pop();
+      }
+      if (arr[i] == '|'){
+        stack.pop();
+      }
+    } else {
+      const bracket = stack.pop();
+      if (bracketsDictionary[bracket] !== arr[i]) {
+        return false;
+      }
+    }
+  }
+
+  return true;
 }
